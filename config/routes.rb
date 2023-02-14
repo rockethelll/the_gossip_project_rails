aa91d1870b6e6c8a4bcc9f20ad8ca2f8305a9152
rockethelll <rockethell@hotmail.com>
2023-02-14 19:27:41 +0100

    ok for today

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  resources :gossip, only: [:new, :create]
-  # get 'author/author'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :gossip, only: [:new, :create]
+  get 'author/author'
   get '/team' => 'team#team'
   get '/contact' => 'contact#contact'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/welcome/:first_name' => 'home#welcome', as: 'welcome'
   get '/show/:id' => 'show#show', as: 'show'
+
   get '/author/:id' => 'author#author', as: 'author'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
