2fea067509891876eab54388087530974b6488b1
rockethelll <rockethell@hotmail.com>
2023-02-16 21:43:32 +0100

    fin  de journee

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,33 +16,3 @@ class SessionsController < ApplicationController
     redirect_to new_session_path
   end
 end
-
-# class SessionsController < ApplicationController
-#   # def new
-#   #   @user = User.new
-#   # end
-
-#   def create
-#     # cherche s'il existe un utilisateur en base avec l’e-mail
-#     puts 'CREATTTEEEEEEEEE'
-#     user = User.find_by(email: params[:email])
-
-#     puts 'PPPAAAAAARRRRRRRRRRAAAAAAAMMMMMSSSSS'
-#     p params
-#     # on vérifie si l'utilisateur existe bien ET si on arrive à l'authentifier (méthode bcrypt) avec le mot de passe
-
-#     if user && user.authenticate(params[:password])
-#       puts 'OOOKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK'
-
-#       session[:user_id] = user.id
-#       redirect_to root_path
-#     else
-#       flash.now[:danger] = 'Invalid email/password combination'
-#       render 'new'
-#     end
-#   end
-
-#   def destroy
-#     redirect_to root_path
-#   end
-# end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
+  ActiveRecord::Base.connection.tables.each do |t|
+    ActiveRecord::Base.connection.reset_pk_sequence!(t)
+  end
   primary_abstract_class
 end
