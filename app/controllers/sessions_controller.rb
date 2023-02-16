009bd43175dc4e90b039c023c3087150d1d20a6d
rockethelll <rockethell@hotmail.com>
2023-02-16 20:43:34 +0100

    logout ok

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,19 @@
 class SessionsController < ApplicationController
 
-  def create 
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to welcome_path(user.first_name)
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to welcome_path(@user.first_name)
     else
-
+      flash.now[:alert] = 'Invalid email/password combination'
       render 'new'
     end
   end
+
   def destroy
     session.delete(:user_id)
-    redirect_to "/sessions/new"
+    redirect_to new_session_path
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :contact, only: [:index]
   resources :city, only: [:show]
   resources :sessions, only: [:new, :create, :destroy]
+  # delete 'logout', to: 'sessions#destroy', as: 'logout'
 
   get '/welcome/:first_name' => 'home#welcome', as: 'welcome'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
